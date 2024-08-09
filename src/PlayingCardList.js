db3261cd9ae99e431e71d8257353c79295ec00fe
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-08-09 13:36:42 -0400

    implement part 3

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -1,28 +1,29 @@
-import React, { useState } from "react";
-import {v1 as uuid} from "uuid";
-import axios from "axios";
+import React from "react";
+import { v1 as uuid } from "uuid";
+// import axios from "axios";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
+import useAxios from "./hooks";
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-  const [cards, setCards] = useState([]);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
-    );
-    setCards(cards => [...cards, { ...response.data, id: uuid() }]);
+  const [cards, addCard] = useAxios(
+    "https://deckofcardsapi.com/api/deck/new/draw/"
+  );
+
+  const addNewCard = () => {
+    addCard(); // No URL ending needed here
   };
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
-        <button onClick={addCard}>Add a playing card!</button>
+        <button onClick={addNewCard}>Add a playing card!</button>
       </div>
       <div className="PlayingCardList-card-area">
-        {cards.map(cardData => (
-          <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
+        {cards.map((cardData) => (
+          <PlayingCard key={uuid()} front={cardData.cards[0].image} />
         ))}
       </div>
     </div>
--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 
 function useFlip() {
   const [isFlipped, setIsFlipped] = useState(true);
@@ -11,3 +12,19 @@ function useFlip() {
 }
 
 export default useFlip;
+
+function useAxios(baseUrl) {
+  const [data, setData] = useState([]);
+
+  const addData = async (urlEnding = "") => {
+    const response = await axios.get(`${baseUrl}${urlEnding}`);
+    setData((data) => [
+      ...data,
+      { ...response.data, id: response.data.id || response.data.name },
+    ]);
+  };
+
+  return [data, addData];
+}
+
+export { useAxios };
